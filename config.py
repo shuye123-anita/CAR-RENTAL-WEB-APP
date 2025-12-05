3309579b08a1c02692f3120b8f8f71324685aea2
shuye123-anita <shuye.berinyu@ictuniversity.edu.cm>
2025-12-05 15:42:28 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Config:
-    SECRET_KEY = 'super-secret-key'
-    NODE_COUNT = 5
-    STORAGE_PER_NODE = 250 * 1024 * 1024  # 250 MB
-    TOTAL_STORAGE = NODE_COUNT * STORAGE_PER_NODE
-    REPLICATION = 2          # Each block saved on 2 nodes
-    BLOCK_SIZE = 64 * 1024    # 64 KB blocks
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///cloud.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    BLOCK_SIZE = 64 * 1024
+    REPLICATION = 2
+    TOTAL_STORAGE = 5 * 250 * 1024 * 1024  # 1.25 GB
